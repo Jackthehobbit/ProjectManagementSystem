9c9650afa6edd7fda80dcd3daabd015b89e66ce0
jack james <scunnyrule45@hotmail.co.uk>
2017-04-07 23:24:29 +0100

    Can now store to the database in the test console app

---
+++ ProjectManagementSystemDatabase/ProjectRepo.cs
@@ -0,0 +1,27 @@
+﻿using ProjectManagementSystemDatabase.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystemDatabase
+{
+    public class ProjectRepo
+    {
+        public ProjectRepo()
+        {
+            ProjectManagementContext con = new ProjectManagementContext();
+
+            Project prj = new Project();
+            prj.Code = "TEST1";
+            prj.Name = "THIS IS A FULL NAME";
+            prj.ShortName = "this is a SNAM";
+            prj.Description = "Desc";
+
+            con.Projects.Add(prj);
+
+            con.SaveChanges();
+        }
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectManagementSystemDatabase;
+using ProjectManagementSystemDatabase.Entities;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            ProjectRepo repo = new ProjectRepo();
+            Console.Read();
+            
+
+        }
+    }
+}
