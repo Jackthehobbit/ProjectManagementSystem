922460f9ffbdd48f08d0eaedbae2d3245620e7d6
jack james <scunnyrule45@hotmail.co.uk>
2017-04-07 22:12:05 +0100

    Including files for inital commit

---
+++ ProjectManagementSystemDatabase/Entities/Project/Project.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectManagementSystemDatabase.Entities.Project
+{
+    public class Project
+    {
+        [Key]
+        [MaxLength(12)]
+        public string Code { get; set; }
+
+        [MaxLength(15)]
+        public string ShortName { get; set; }
+
+        [MaxLength(120)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+    }
+}
---
+++ ProjectManagementSystemDatabase/ProjectManagementContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ProjectManagementSystemDatabase.Entities.Project;
+
+namespace ProjectManagementSystemDatabase
+{
+    public class ProjectManagementContext
+    {
+        public ProjectManagementContext() : base("ProjectManagementConnectionString")
+        {
+
+        }
+
+        public DbSet<Project> Project { get; set; }
+    }
+}
